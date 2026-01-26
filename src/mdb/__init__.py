174701cf29aa54b36aa64cc6b1fade7a6eb939a3
Belmar <matheusleal2596@gmail.com>
2026-01-26 00:47:54 -0300

    build: tests

---
+++ src/mdb/__init__.py
@@ -0,0 +1,4 @@
+from mdb.adapters.scrapers.ibge.ibge_scraper import IBGEScraper
+from mdb.adapters.scrapers.sgs.bcb_scraper import BACENScraper
+
+__all__ = ["IBGEScraper", "BACENScraper"]
--- src/mdb/adapters/scrapers/ibge/ibge_scraper.py
+++ src/mdb/adapters/scrapers/ibge/ibge_scraper.py
@@ -1,18 +1,16 @@
 import requests as req
-from macrodatahub.adapters.outbound.storage.local_storage import LocalStorageAdapter as LSA
 
 class IBGEScraper:
     BASE_URL = 'https://apisidra.ibge.gov.br/'
 
     def __init__(
             self, 
-            code:  str, 
-            path:  str,
-            cache: bool = False):
+            code:          str, 
+            landing_path:  str
+            ):
         
         self.code = code
-        self.Raw_path = path
-        self.Storage = LSA()
+        self.Landing_path = landing_path
 
     def build_request(self) -> str:
         if not self.code:
@@ -20,14 +18,16 @@ class IBGEScraper:
         format_suffix = f"values/{self.code}?formato=json"
         return self.BASE_URL + format_suffix
 
-    def fetch(self) -> list:
-        url = self.build_request()
+    def fetch(self, 
+              url: str
+              ) -> None:
         response = req.get(url)
         #response.raise_for_status()
         return response.json()
 
     def Scrap(self):
-        Response=self.fetch()
+        request=self.build_request()
+        self.fetch(request)
         self.Storage.save_raw(
                 base_path=self.Raw_path,
                 data=Response,
